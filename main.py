c650e728bc0c74a75dec28f6a72d18b2979bc0b3
Danila Abramov <67269761+danilaabramov@users.noreply.github.com>
2023-10-15 20:26:14 +0400

    Add files via upload

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from fastapi.middleware.cors import CORSMiddleware
 from sockets import sio_app, chat
 import asyncio
 import concurrent.futures
+from pydantic import BaseModel
 
 app = FastAPI()
 
@@ -17,11 +18,17 @@ def chat_with_gpt3(message):
     for token in response:
         asyncio.run(chat('gpt', token))
 
-@app.get("/{message}")
-async def home(message: str):  # Define a path parameter 'message'
+
+class MessageInput(BaseModel):
+    message: str
+
+@app.post("/chat")
+async def home(data: MessageInput):
+    message = data.message
+    await chat('я', message)
     loop = asyncio.get_event_loop()
     with concurrent.futures.ThreadPoolExecutor() as pool:
-        await loop.run_in_executor(pool, chat_with_gpt3, message)  # Pass the 'message' parameter to chat_with_gpt3
+        await loop.run_in_executor(pool, chat_with_gpt3, message)
 
     return {"status": "processing"}
 
@@ -34,6 +41,7 @@ app.add_middleware(
     allow_methods=['*'],
     allow_headers=['*']
 )
+
 
 if __name__ == '__main__':
     uvicorn.run('main:app', reload=True)
